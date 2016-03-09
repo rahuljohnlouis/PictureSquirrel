dbb2aedb891005c01b5cdc42e23540392e2cc4a3
Rahul John Louis <rahul.louis@gmail.com>
2016-03-08 20:06:51 -0500

    Added Data Source with images table and pics folder

--- WebContent/home.jsp
+++ WebContent/home.jsp
@@ -1,6 +1,16 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/sql" prefix="sql"%>
 <c:import url="header.jsp">
 <c:param name="title" value="Picture Squirrel Home"></c:param>
 </c:import>
-Content goes here
+<!-- Set data source -->
+<sql:setDataSource var="ds" dataSource="jdbc/webshop"/>
+
+<sql:query dataSource="${ds}" sql="select * from images limit 10" var="results" />
+<!--  Use for each loop and access rows from the var results above -->
+<c:forEach var="image" items="${results.rows }">
+
+<p>${image.stem}.${image.image_extension }</p>
+
+</c:forEach>
 <c:import url="footer.jsp"></c:import>
--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,7 +1,6 @@
 package controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,34 +8,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Controller
- */
 @WebServlet("/Controller")
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Controller() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	public Controller() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.getRequestDispatcher("/home.jsp").forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
